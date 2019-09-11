106980b3a91e4b9091347e9b59a0fd7c80a836e5
Cassidy Christian <cassidysparkles@icloud.com>
2019-09-11 13:11:59 -0600

    Add ./

---
+++ Chapter19/RunofIntegers.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class RunofIntegers
+{
+    public static void main ( String[] args )
+    {
+        Scanner scan = new Scanner(System.in);
+
+int count, limit;  
+
+System.out.println("Enter start:");
+count = scan.nextInt();
+
+System.out.println("Enter end:");
+limit = scan.nextInt();
+
+while ( count <= limit )  // there are changes here
+{
+  System.out.println( count );
+  count = count + 1;   
+}
+}
+}
