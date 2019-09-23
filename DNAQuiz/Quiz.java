bbbc193da6429457ae80e36d20ff33a3920ac234
Cassidy Christian <cassidysparkles@icloud.com>
2019-09-22 18:13:12 -0600

    Add ../DNAQuiz/

---
+++ DNAQuiz/Quiz.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Quiz
+{
+    public static void main ( String[] args )
+    {
+        String string = new String (System.in);
+        Scanner scan= new Scanner (System.in); 
+    String nucleotide, new_string, new_nucleotide; 
+    System.out.println("Enter Nucleotide");
+    nucleotide= scan.nextstring();
+
+    while (nucleotide==T || nucleotide==A || nucelotide==C || nucleotide==G)
+    {
+        Print.out.nextln("Enter Nucleotide"); 
+        new_nucleotide= scan.nextstring(); 
+        new_string=nucleotide;
+        new_string= new_string+new_nucleotide; 
+    } 
+        System.out.println(new_string); 
+
+    }
+}
