8e4c96136c78f1b39e507194d14c1082120330ae
Cassidy Christian <cassidysparkles@icloud.com>
2019-11-08 12:04:09 -0700

    Chapter52

--- Chapter53/Pantry.java
+++ Chapter53/Pantry.java
@@ -14,33 +14,66 @@ class Pantry
     this . jar3 = jar3 ;
     selected = null ;
   }
+  Pantry(Jam jar1, Jam jar2){
+      this.jar1=jar1; 
+      this.jar2=jar2;
+      this.jar3=null; 
+      selected=null; 
+      
+  }
 
   // Methods
   public String toString()
   {
     String str = "";
-    str += "1: " +  jar1.toString()  + "\n";
-    str += "2: " +  jar2.toString()  + "\n";
-    str += "3: " +  jar3.toString()  + "\n";     
+    
+    if (jar1 != null)
+        str += "1: " +  jar1.toString()  + "\n";
+    
+    if (jar2 != null)
+        str += "2: " +  jar2.toString()  + "\n";
+    
+    if (jar3!=null)
+        str += "3: " +  jar3.toString()  + "\n"; 
+    
     return str;
   }
+  public void print(){
+      System.out.println(this.toString());
+  }
+  public void replace(Jam j, int slot){
+    if (slot==1)
+      jar1=j; 
+    if(slot==2)
+    jar2=j; 
+    if(slot==3)
+    jar3=j; 
+}
 
   // assume that the user entered a correct selection, 1, 2, or 3
-  public void select( int jarNumber )
+  public boolean select( int jarNumber )
   {
-    if ( jarNumber == 1 )
-      selected =  jar1 ;
-
-    else if ( jarNumber == 2 )
+    if ( jarNumber == 1 && jar1 != null){
+      selected = jar1;
+      return true; 
+    }
+    else if ( jarNumber == 2 && jar2!=null){
       selected = jar2 ;
-
-    else 
-      selected = jar3 ;
+      return true; 
+    }
+    else if ( jarNumber == 3 && jar3!=null){
+        selected =jar3; 
+        return true; 
+    }
+      else 
+        return false; 
+      
   }
 
   // spread the selected jam
   public void spread( int oz )
   {
+    if (selected != null) 
     selected . spread( oz ) ;
   
 }
