fcefab980eb3d1fb0adb76b437a847dbafb596ae
Cassidy Christian <cassidysparkles@icloud.com>
2019-12-06 09:56:20 -0700

    Add WeatherinfoTester.java

---
+++ Chapter63/WeatherinfoTester.java
@@ -0,0 +1,8 @@
+class WeatherinfoTester{
+    public static void main ( String[] args ) {
+
+Weatherinfo destination= new Weatherinfo (200, 30); 
+
+System.out.println("Temperature:"+destination.gettemp()+"GeoLocation:"+destination.getgeoinfo());
+    }
+}
